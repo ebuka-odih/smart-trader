dfc86c541c24971263813c40815f47c44e7e3408
Gnosis <emmagab38@gmail.com>
2025-08-25 01:05:21 +0100

    commit changes

--- resources/views/dashboard/layout/app.blade.php
+++ resources/views/dashboard/layout/app.blade.php
@@ -50,18 +50,7 @@
                 </div>
             </div>
 
-            <!-- Language & Theme Settings -->
-            <div class="p-4 border-b border-gray-700">
-                <div class="flex items-center justify-between mb-3">
-                    <span class="text-gray-400 text-sm">LANGUAGE: EN</span>
-                </div>
-                <div class="flex items-center space-x-2">
-                    <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"></path>
-                    </svg>
-                    <span class="text-gray-300 text-sm">Light</span>
-                </div>
-            </div>
+
 
             <!-- Navigation Menu -->
             <nav class="flex-1 p-4 space-y-4 overflow-y-auto">
