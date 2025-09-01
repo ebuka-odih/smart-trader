aa12697dc11a8674ccffe18cc6df38ec730b8569
Gnosis <emmagab38@gmail.com>
2025-09-01 22:09:20 +0100

    commit changes

--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -12,7 +12,7 @@
     <meta name="locale" content="en">
     <meta name="content-language" content="en">
     <title>BlackRock | Home</title>
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>{{ config('app.name')[0] }}</text></svg>" type="image/svg+xml" />
+    <link rel="icon" href="storage/settings/l21Oj4xbtOpQp7BGToYlVcaPz1vISLAqr1vb5bwB.png" type="image/png" />
     <script src="https://cdn.tailwindcss.com/"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/dark.css">
@@ -254,8 +254,8 @@
 <!-- Add this loader HTML right after the body tag -->
 <div id="site-loader" class="fixed inset-0 flex items-center justify-center bg-[#0A0714] z-50">
     <div class="flex flex-col items-center">
-        <div class="w-32 h-32 mb-4 bg-[#2FE6DE] rounded-full flex items-center justify-center">
-            <span class="text-4xl font-bold text-black">{{ config('app.name') }}</span>
+        <div class="w-32 h-32 mb-4">
+            <img src="storage/settings/bv05CPQdQQcgRWpEAg1CMcA5t2CUohmbPg0XJXUD.png" alt="Loading..." class="animate-pulse w-full h-full object-contain">
         </div>
 
     </div>
