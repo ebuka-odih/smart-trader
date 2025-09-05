14871eade01223ca4d8f8310cf254eb6e60a8146
Gnosis <emmagab38@gmail.com>
2025-09-05 23:47:00 +0100

    Fix duplicate notification issue - Add duplicate prevention logic to all notification listeners

--- resources/views/dashboard/layout/app.blade.php
+++ resources/views/dashboard/layout/app.blade.php
@@ -402,8 +402,11 @@
                             <span>Copy Trading</span>
                         </a>
                         <a href="{{ route('user.botTrading.index') }}" class="flex items-center space-x-3 px-3 py-1.5 rounded-lg {{ request()->routeIs('user.botTrading.*') ? 'bg-blue-600 text-white' : 'text-gray-300 hover:bg-gray-700' }}">
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd"></path>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="w-5 h-5" fill="currentColor">
+                                <path d="M21.928 11.607c-.202-.488-.635-.605-.928-.633V8c0-1.103-.897-2-2-2h-6V4.61c.305-.274.5-.668.5-1.11a1.5 1.5 0 0 0-3 0c0 .442.195.836.5 1.11V6H5c-1.103 0-2 .897-2 2v2.997l-.082.006A1 1 0 0 0 1.99 12v2a1 1 0 0 0 1 1H3v5c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5a1 1 0 0 0 1-1v-1.938a1 1 0 0 0-.072-.455M5 20V8h14l.001 3.996L19 12v2l.001.005l.001 5.995z"/>
+                                <ellipse cx="8.5" cy="12" fill="currentColor" rx="1.5" ry="2"/>
+                                <ellipse cx="15.5" cy="12" fill="currentColor" rx="1.5" ry="2"/>
+                                <path fill="currentColor" d="M8 16h8v2H8z"/>
                             </svg>
                             <span>Bot Trading</span>
                         </a>
